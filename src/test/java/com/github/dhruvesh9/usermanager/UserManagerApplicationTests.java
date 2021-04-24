a926282cc8f3312c2fe5cbe32d58bbcdd308789a
Dhruvesh Navghare <dun814@gmail.com>
2021-04-24 15:04:08 +0530

    Created a spring boot application, initial setup

---
+++ src/test/java/com/github/dhruvesh9/usermanager/UserManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dhruvesh9.usermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
